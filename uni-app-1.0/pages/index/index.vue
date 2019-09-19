c31801782ceee432c5f3662645df6c80bbe32565
孙悦 <1401398047@qq.com>
2019-09-19 10:52:16 +0800

    tj

---
+++ uni-app-1.0/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<view class="uni-list">
+			<!-- <navigator url="../info/info"> -->
+				<view 
+					class="uni-list-cell" 
+					hover-class="uni-list-cell-hover" 
+					v-for="(item, index) in news" 
+					:key="index" 
+					@tap="openinfo" 
+					:data-newsid="item.post_id">
+					<view class="uni-media-list">
+						<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+						<view class="uni-media-list-body">
+							<view class="uni-media-list-text-top">{{ item.title }}</view>
+							<view class="uni-media-list-text-bottom uni-ellipsis">{{ item.created_at }}</view>
+						</view>
+					</view>
+				</view>
+			<!-- </navigator> -->
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			news:[]
+		};
+	},
+	//表示页面加载完毕
+	onLoad() {
+		uni.request({
+			url: 'https://unidemo.dcloud.net.cn/api/news', //仅为示例，并非真实接口地址。
+			data: {},
+			header: {},
+			success: res => {
+				this.news = res.data;
+			}
+		});
+	},
+	methods: {
+		openinfo(e){
+			let newsid = e.currentTarget.dataset.newsid;
+			//页面跳转
+			uni.navigateTo({
+				url: '../info/info?newsid='+newsid
+			});
+		}
+	}
+};
+</script>
+
+<style>
+	.uni-media-list-body{
+		height:auto;
+	}
+	.uni-media-list-text-top{
+		line-height:1.6em;
+	}
+</style>
---
+++ uni-app-1.0/pages/info/info.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}</view>
+		<rich-text class="richText" :nodes='string'></rich-text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'',
+				string:''
+			}
+		},
+		onLoad(e) {
+			//获取到前一个页面传递的数据
+			let newsid = e.newsid;
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+newsid, //仅为示例，并非真实接口地址。
+				data: {},
+				header: {},
+				success: res => {
+					console.log(res.data);
+					this.title = res.data.title;
+					this.string = res.data.content;
+				}
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content{
+		padding:10upx 2%;
+		width:96%;
+		flex-wrap: warp;
+	}
+	.title{
+		line-height: 2em;
+		font-weight: 700;
+		font-size: 38upx;
+	}
+</style>
